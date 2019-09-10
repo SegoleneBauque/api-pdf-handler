599046b1e0b674c31bc68b290cef92569d3439ea
Segolene Bauque <sego.bauque@gmail.com>
2019-09-10 23:50:20 +0200

    transformation du projet en Web Service RESTfull

--- src/main/java/fr/epsi/projet/api/pdf/handler/AfficherPdf.java
+++ src/main/java/fr/epsi/projet/api/pdf/handler/AfficherPdf.java
@@ -6,9 +6,15 @@
 package fr.epsi.projet.api.pdf.handler;
 
 import com.itextpdf.forms.PdfPageFormCopier;
+import com.itextpdf.io.image.ImageData;
+import com.itextpdf.io.image.ImageDataFactory;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfReader;
 import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Image;
+import com.itextpdf.styledxmlparser.css.media.MediaType;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -16,130 +22,155 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.UriInfo;
 
 /**
  *
  * @author mjj19
  */
-public class AfficherPdf extends HttpServlet {
+@Path("pdf")
+public class AfficherPdf {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("application/pdf");
-        // récupération du nom du fichier à afficher dans les paramètres de la requête :
-        String fileName = request.getParameter("name");
-        // recuperation du type d'operation qu'on souhaite realiser avec le pdf (view, split, concatene) :
-        String typeOperation = request.getParameter("typeOperation");
-        if (typeOperation == null || "view".equals(typeOperation)) {
-            viewPdf(fileName, response.getOutputStream());
-        } else if ("concatene".equals(typeOperation)) {
-            String fileName2 = request.getParameter("name2");
-            OutputStream outputStream = response.getOutputStream();
-            concatenePdf(fileName, fileName2, outputStream);
-        } else if ("split".equals(typeOperation)) {
-            int startPage = Integer.parseInt(request.getParameter("startPage"));
-            int endPage = Integer.parseInt(request.getParameter("endPage"));
-            OutputStream outputStream = response.getOutputStream();
-            splitPdf(fileName, outputStream, startPage, endPage);
-        }
+    @Context
+    private UriInfo context;
+
+    public AfficherPdf() {
     }
 
-    private void concatenePdf(String fileName, String fileName2, OutputStream outputStream) throws IOException {
-        // création de la variable pdf avec le chemin absolu du fichier concerné :
+    @GET
+    @Path("concatene") // http://localhost:8080/api-pdf-handler/resources/pdf/concatene?name=resultat.pdf&name2=pdf-sample.pdf
+    @Produces("application/pdf")
+    public Response concatenePdf(@QueryParam("name") String fileName, @QueryParam("name2") String fileName2) {
+        // creation de la variable pdf avec le chemin absolu des fichiers concernes :
         File pdf = new File("C:\\test\\pdf\\" + fileName);
         File pdf2 = new File("C:\\test\\pdf\\" + fileName2);
         // lecture des fichiers :
-        PdfDocument pdfDocument = new PdfDocument(new PdfReader(pdf));
-        PdfDocument pdfDocument2 = new PdfDocument(new PdfReader(pdf2));
-        
-        // création de l'objet pour écrire le pdf :
-        PdfDocument pdfWriter = new PdfDocument(new PdfWriter(outputStream));
-        // copie de pdfDocument et pdfDocument2 dans pdfWriter :
-        pdfDocument.copyPagesTo(1, pdfDocument.getNumberOfPages(), pdfWriter, new PdfPageFormCopier());
-        pdfDocument2.copyPagesTo(1, pdfDocument2.getNumberOfPages(), pdfWriter, new PdfPageFormCopier());
-        // fermeture des flux :
-        pdfDocument.close();
-        pdfDocument2.close();
-        pdfWriter.close();
+        try {
+            PdfDocument pdfDocument = new PdfDocument(new PdfReader(pdf));
+            PdfDocument pdfDocument2 = new PdfDocument(new PdfReader(pdf2));
+            // creation d'un tableau de bytes
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+            // creation de l'objet pour ecrire le pdf et le transformer en tableau de bytes :
+            PdfDocument pdfWriter = new PdfDocument(new PdfWriter(baos));
+            // copie de pdfDocument et pdfDocument2 dans pdfWriter :
+            pdfDocument.copyPagesTo(1, pdfDocument.getNumberOfPages(), pdfWriter, new PdfPageFormCopier());
+            pdfDocument2.copyPagesTo(1, pdfDocument2.getNumberOfPages(), pdfWriter, new PdfPageFormCopier());
+            // fermeture des flux :
+            pdfDocument.close();
+            pdfDocument2.close();
+            pdfWriter.close();
+            baos.close();
+
+            return Response
+                    .status(Response.Status.OK)
+                    .type("application/pdf")
+                    .header("Content-Disposition", "filename=" + fileName)
+                    .entity(baos.toByteArray())
+                    .build();
+        } catch (IOException e) {
+            return Response.status(Response.Status.NO_CONTENT)
+                    .build();
+        }
     }
 
-    private void splitPdf(String fileName, OutputStream outputStream, int startPage, int endPage) throws IOException {
+    @GET
+    @Path("split") // http://localhost:8080/api-pdf-handler/resources/pdf/split?name=pdf-sample.pdf&startPage=2&endPage=3
+    @Produces("application/pdf")
+    public Response splitPdf(@QueryParam("name") String fileName, @QueryParam("startPage") int startPage, @QueryParam("endPage") int endPage) {
+        // création de la variable pdf avec le chemin absolu du fichier concerné :
+        File pdf = new File("C:\\test\\pdf\\" + fileName);
+        // lecture du fichier :
+        try {
+            PdfDocument pdfDocument = new PdfDocument(new PdfReader(pdf));
+            // creation d'un tableau de bytes
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+            // création de l'objet pour écrire le pdf :
+            PdfDocument pdfDocument2 = new PdfDocument(new PdfWriter(baos));
+            // copie du pdfDocument dans pdfDocument2 :
+            pdfDocument.copyPagesTo(startPage, endPage, pdfDocument2, new PdfPageFormCopier());
+            // fermeture des flux :
+            pdfDocument.close();
+            pdfDocument2.close();
+            baos.close();
+            return Response
+                    .status(Response.Status.OK)
+                    .type("application/pdf")
+                    .header("Content-Disposition", "filename=" + fileName)
+                    .entity(baos.toByteArray())
+                    .build();
+        } catch (IOException e) {
+            return Response.status(Response.Status.NO_CONTENT)
+                    .build();
+        }
+    }
+
+    @GET
+    @Path("view") // http://localhost:8080/api-pdf-handler/resources/pdf/view?name=pdf-sample.pdf
+    @Produces("application/pdf")
+    public Response viewPdf(@QueryParam("name") String fileName) throws IOException {
         // création de la variable pdf avec le chemin absolu du fichier concerné :
         File pdf = new File("C:\\test\\pdf\\" + fileName);
         // lecture du fichier :
         PdfDocument pdfDocument = new PdfDocument(new PdfReader(pdf));
+        // creation d'un tableau de bytes
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
         // création de l'objet pour écrire le pdf :
-        PdfDocument pdfDocument2 = new PdfDocument(new PdfWriter(outputStream));
-        // copie du pdfDocument dans pdfDocument2 :
-        pdfDocument.copyPagesTo(startPage, endPage, pdfDocument2, new PdfPageFormCopier());
-        // fermeture des flux :
-        pdfDocument.close();
-        pdfDocument2.close();
-    }
-
-    private void viewPdf(String fileName, OutputStream outputStream) throws IOException {
-        // création de la variable pdf avec le chemin absolu du fichier concerné :
-        File pdf = new File("C:\\test\\pdf\\" + fileName);
-        // lecture du fichier :
-        PdfDocument pdfDocument = new PdfDocument(new PdfReader(pdf));
-
-        // création de l'objet pour écrire le pdf :
-        PdfDocument pdfDocument2 = new PdfDocument(new PdfWriter(outputStream));
+        PdfDocument pdfDocument2 = new PdfDocument(new PdfWriter(baos));
         // copie du pdfDocument dans pdfDocument2 :
         pdfDocument.copyPagesTo(1, pdfDocument.getNumberOfPages(), pdfDocument2, new PdfPageFormCopier());
         // fermeture des flux :
         pdfDocument.close();
         pdfDocument2.close();
+        baos.close();
+        return Response
+                .status(Response.Status.OK)
+                .type("application/pdf")
+                .header("Content-Disposition", "filename=" + fileName)
+                .entity(baos.toByteArray())
+                .build();
     }
 
-// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+    @GET
+    @Path("addImage") // http://localhost:8080/api-pdf-handler/resources/pdf/addImage?name=pdf-sample.pdf&name2=sego.png
+    @Produces("application/pdf")
+    public Response addImageToPdf(@QueryParam("name") String fileName, @QueryParam("name2") String imageName) throws IOException {
+        // création de la variable pdf avec le chemin absolu du fichier concerné :
+        File pdf = new File("C:\\test\\pdf\\" + fileName);
+        
+        // creation d'un tableau de bytes
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        // modification du PDF et enregistrement en outputStream :
+        PdfDocument pdfDocument = new PdfDocument(new PdfReader(pdf), new PdfWriter(baos));
+        // Document sur lequel on veut ajouter un élément (paragraphes, images etc.) :
+        Document document = new Document(pdfDocument);
+
+        // chargement de l'image sur le disque
+        ImageData imageData = ImageDataFactory.create("C:\\test\\pdf\\" + imageName);
+        // creation d'une image et fourniture des parametres. Page numero = 1 :
+        Image image = new Image(imageData).scaleAbsolute(50 * imageData.getWidth() / imageData.getHeight(), 50).setFixedPosition(1, 25, 25);
+        // Ajout de l'image sur la page
+        document.add(image);
+
+        // Clôture du flux :
+        document.close();
+        baos.close();
+        return Response
+                .status(Response.Status.OK)
+                .type("application/pdf")
+                .header("Content-Disposition", "filename=" + fileName)
+                .entity(baos.toByteArray())
+                .build();
     }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
