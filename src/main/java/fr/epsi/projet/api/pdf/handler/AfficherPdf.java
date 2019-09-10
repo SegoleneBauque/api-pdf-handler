9886b4455d0cc455a1a522816414cdf961fd8ea5
Segolene Bauque <sego.bauque@gmail.com>
2019-09-10 20:58:17 +0200

    Ajout fonction apposition d'une image

--- src/main/java/fr/epsi/projet/api/pdf/handler/AfficherPdf.java
+++ src/main/java/fr/epsi/projet/api/pdf/handler/AfficherPdf.java
@@ -12,9 +12,7 @@ import com.itextpdf.kernel.pdf.PdfWriter;
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -34,6 +32,7 @@ public class AfficherPdf extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
+    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("application/pdf");
